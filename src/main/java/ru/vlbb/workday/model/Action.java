3bd5506ae5a55b4bfebf83dfe91ff6e4ed1a020a
Ivan Soldatov <38758452+ivansoldatov@users.noreply.github.com>
2022-10-28 21:17:59 +0300

    feat: add Action

---
+++ src/main/java/ru/vlbb/workday/model/Action.java
@@ -0,0 +1,41 @@
+package ru.vlbb.workday.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Action {
+    private final LocalDate date;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+    private final int duration;
+    private final String description;
+
+    public Action(LocalDate date, LocalTime startTime, LocalTime endTime, int duration, String description) {
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.duration = duration;
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
